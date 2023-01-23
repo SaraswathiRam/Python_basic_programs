529217580064526395aa3b720cbbcfc1ac3d74eb
SaraswathiRam <sairam11saras@gmail.com>
2023-01-22 20:55:38 -0500

    First Commit

---
+++ Ascii.py
@@ -0,0 +1,8 @@
+#to print ascii value of a character 
+a = 'g'
+print("The ascii value of " , a , "is" , ord(a))
+
+
+"""c = 'g'
+# print the ASCII value of assigned character in c
+print("The ASCII value of '" + c + "' is", ord(c))"""
