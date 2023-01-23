e9660b15d6afb87584340b628103036e2d0d96a7
SaraswathiRam <sairam11saras@gmail.com>
2023-01-22 21:06:21 -0500

    Add2nos

---
+++ add2nos.py
@@ -0,0 +1,5 @@
+#python program to add 2 nos 
+number1 = int(input("Pls enter num1 :"))
+number2 = int(input("Pls enter num2 :"))
+result = number1 + number2
+print("The sum of 2 nos is ," , result)
